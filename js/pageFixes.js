45f6a8a6ac3064d17bd6d5ee385278106b4ceaf5
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-04-30 12:47:36 -0400

    Accidentally checked in DEV codebase.

--- js/pageFixes.js
+++ js/pageFixes.js
@@ -1,17 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************************!*\
-  !*** ./src/content/pageFixes.js ***!
-  \**********************************/
-(async() => {
-  let link = document.querySelector('[aria-label="Pages. Disabled. Not visible to students"]');
-  if (link) {
-    let url = link.getAttribute('href');
-    url = url.replace('wiki', 'pages');
-    link.setAttribute("href", url);
-  }
-
-})();
-/******/ })()
-;
+(async()=>{let e=document.querySelector('[aria-label="Pages. Disabled. Not visible to students"]');if(e){let t=e.getAttribute("href");t=t.replace("wiki","pages"),e.setAttribute("href",t)}})();
 //# sourceMappingURL=pageFixes.js.map
